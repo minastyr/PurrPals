004f0f3d70a3511121e714c3ed1cc96294147399
AnnuArchon <minastyr@gmail.com>
2025-02-24 20:07:10 -0500

    package.json additions

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS (if using react-bootstrap)
-import App from './App';
+import App from './app';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,6 @@
 const forceDatabaseRefresh = false;
 const dotenv = require('dotenv');
+const path = require('path');
 
 dotenv.config();
 
@@ -11,10 +12,18 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Serves static files in the entire client's dist folder
-app.use(express.static('../client'));
+// app.use(express.static('../client/'));
+// app.use(express.json());
+// app.use(routes);
 
+app.use(express.static(path.join(__dirname, '../client/public')));
 app.use(express.json());
 app.use(routes);
+
+//test route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../Client/src'));
+});
 
 sequelize.sync({force: forceDatabaseRefresh}).then(() => {
   app.listen(PORT, () => {
