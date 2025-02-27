21839f2e816957680245ddf60944d68f8ecfce62
AnnuArchon <minastyr@gmail.com>
2025-02-27 14:46:52 -0500

    revisiting the index.js app entry again

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from '../src/app';
+import App from './app';
 
 // Load Google Fonts (Roboto)
 import './index.css'; 
