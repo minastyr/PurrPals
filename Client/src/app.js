9c72966a6a9d6116d4b35fec77ac51908e6afee2
AnnuArchon <minastyr@gmail.com>
2025-02-27 17:02:11 -0500

    closer exam of index.js p2

--- Client/src/app.js
+++ Client/src/app.js
@@ -1,4 +1,4 @@
-/* import React from 'react';
+import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
@@ -6,33 +6,33 @@ import PetOfDay from './components/PetOfDay';
 import FindAPal from './components/FindAPal';
 import BreedInfo from './components/BreedInfo';
 import SavedPets from './components/SavedPets';
-import Footer from './components/Footer'; */
+import Footer from './components/Footer';
 
-function App() {
-  // return (
-  //   <Router>
-  //     <div className="App">
-  //       <Header />
-  //       <div className="container-fluid py-4" style={{ backgroundColor: '#F5F7FA' }}>
-  //         <div className="row justify-content-center">
-  //           <div className="col-12 col-md-10">
-  //             <PetOfDay />
-  //             <FindAPal />
-  //             <BreedInfo />
-  //             <SavedPets />
-  //           </div>
-  //         </div>
-  //       </div>
-  //       <Footer />
-  //     </div>
-  //   </Router>
-  // );
+function app() {
   return (
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container-fluid py-4" style={{ backgroundColor: '#F5F7FA' }}>
+          <div className="row justify-content-center">
+            <div className="col-12 col-md-10">
+              <PetOfDay />
+              <FindAPal />
+              <BreedInfo />
+              <SavedPets />
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
+/*   return (
     <div>
       <h1>Welcome to PurrPals App API!</h1>
     </div>
-  );
+  ); */
 
 }
 
-export default App;
+export default app;
--- Client/src/index.js
+++ Client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { App } from './app';
+import { app } from './app';
 
 
 // Load Google Fonts (Roboto)
@@ -10,4 +10,4 @@ import './index.css';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
-root.render(<App />);
+root.render(<app />);
