71a09edff886584b5993f1ee76df427a81638707
AnnuArchon <minastyr@gmail.com>
2025-02-27 13:56:44 -0500

    vite filename path change using Toshies startup

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     outDir: 'build',
     rollupOptions: {
-      input: 'public/index.html',
+      input: './client/public/index.html',
     },
   },
   plugins: [react()],
