dad7720765e0fba46b41a0ac916ef5532cd58ab0
AnnuArchon <minastyr@gmail.com>
2025-02-27 15:00:02 -0500

    revisiting the index.js app entry again again again

--- Client/src/app.js
+++ Client/src/app.js
@@ -9,30 +9,30 @@ import SavedPets from './components/SavedPets';
 import Footer from './components/Footer';
 
 function App() {
+  // return (
+  //   <Router>
+  //     <div className="App">
+  //       <Header />
+  //       <div className="container-fluid py-4" style={{ backgroundColor: '#F5F7FA' }}>
+  //         <div className="row justify-content-center">
+  //           <div className="col-12 col-md-10">
+  //             <PetOfDay />
+  //             <FindAPal />
+  //             <BreedInfo />
+  //             <SavedPets />
+  //           </div>
+  //         </div>
+  //       </div>
+  //       <Footer />
+  //     </div>
+  //   </Router>
+  // );
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <div className="container-fluid py-4" style={{ backgroundColor: '#F5F7FA' }}>
-          <div className="row justify-content-center">
-            <div className="col-12 col-md-10">
-              <PetOfDay />
-              <FindAPal />
-              <BreedInfo />
-              <SavedPets />
-            </div>
-          </div>
-        </div>
-        <Footer />
-      </div>
-    </Router>
-  );
-/*   return (
     <div>
-      <h1>Welcome to PurrPals</h1>
+      <h1>Welcome to PurrPals App API!</h1>
     </div>
   );
- */
+
 }
 
 export default App;
