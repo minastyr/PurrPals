9d8364db1f6467de68fa11cb7d5a4b99b531753a
AnnuArchon <minastyr@gmail.com>
2025-02-27 13:32:09 -0500

    vite filename path change 1

---
+++ Client/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  root: 'Client',
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      input: '../client/public/index.html',
+    },
+  },
+  plugins: [react()],
+});
