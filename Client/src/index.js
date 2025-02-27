bbcd686d50a86578e934bb760f676b153c94838e
AnnuArchon <minastyr@gmail.com>
2025-02-27 18:02:01 -0500

    Still borked..but cleaned up dependencies

--- Client/src/index.js
+++ Client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from "react-dom/client";
 // import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { App } from './app';
+import App from './app';
 
 
 // Load Google Fonts (Roboto)
